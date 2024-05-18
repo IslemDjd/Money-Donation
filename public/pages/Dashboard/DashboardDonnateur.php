9fceab07b1b109fb82ae397b32792532b323422b
Islem Dj <islemas23@gmail.com>
2024-05-18 20:34:08 +0100

    Updates

--- public/pages/Dashboard/DashboardDonnateur.php
+++ public/pages/Dashboard/DashboardDonnateur.php
@@ -1,12 +1,18 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Dashboard </title>
     <link rel="stylesheet" href="../index.css">
 </head>
 <body>
-    Dashboard
+    <?php 
+        include "../NavBar/NavBar.php";
+    ?>
+    Dashboard Donnateur
 </body>
 </html>
