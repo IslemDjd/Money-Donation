cc6047eca6db61a6b51a98478ee315b88f7eb770
Islem Dj <islemas23@gmail.com>
2024-05-19 17:58:31 +0100

    Updates

--- public/pages/myProjects.php
+++ public/pages/myProjects.php
@@ -16,8 +16,7 @@ $id=$_SESSION['id'];
 </head>
 <body class="bg-gray-100">
    
-    
-    <div class="grid gap-6 p-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid gap-6 w-full h-full p-4 mt-16 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
         <!-- Project Card -->
 
         <?php $get_projects=mysqli_query($connfig,"SELECT p.* , u.* FROM `projects` p JOIN `users` u ON p.id_user=u.id_user WHERE u.id_user='$id' order by p.date desc");
@@ -25,17 +24,14 @@ $id=$_SESSION['id'];
             {
                 while($row=mysqli_fetch_assoc($get_projects))
                  {
-                    echo'<a href="#">
+                    echo'
                     <div class="bg-white rounded-lg shadow-md relative">
-                    <img class="w-full h-60 object-cover rounded-t-lg" alt="Card Image" src="./pfp_project/'.$row['project_photo'].'">
+                    <img class="w-full  sm:h-60 lg:h-80 object-cover rounded-t-lg" alt="Card Image" src="./pfp_project/'.$row['project_photo'].'">
                     <div class="p-4 ">
                     <h2 class="text-xl font-bold">Project : '.$row['project_name'].'</h2> 
-                    
                     <h3 class="text-lg font-semibold mt-3 ">Montant : <span class="text-green-600"> '.$row['Objectif'].'</span>DA</h3>
-                    
-                
                     </div>
-                    </div></a>';
+                    </div>';
                 }
            }
        ?>
